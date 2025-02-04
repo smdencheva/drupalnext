f050aeb0fb242aa067e2b7473331c8d39606fd6c
Stanislava Dencheva <smdencheva@gmail.com>
2025-02-04 11:52:59 +0200

    Fix settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -854,6 +854,8 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # $settings['migrate_file_public_path'] = '';
 # $settings['migrate_file_private_path'] = '';
 
+$settings['config_sync_directory'] = '../config/sync';
+
 /**
  * Load local development override configuration, if available.
  *
@@ -867,20 +869,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  *
  * Keep this code block at the end of this file to take full effect.
  */
-#
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
-$databases['default']['default'] = array (
-  'database' => 'database',
-  'username' => 'mysql',
-  'password' => 'mysql',
-  'prefix' => '',
-  'host' => 'myservice',
-  'port' => '3306',
-  'isolation_level' => 'READ COMMITTED',
-  'driver' => 'mysql',
-  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-);
-$settings['config_sync_directory'] = 'sites/default/files/config_uSISslTdlpsfnIEqwEiyrNPwHqY68MaFW7GhokmB5pYNfNg3CHUvAqlK4MllQSkxCAmFujFDJw/sync';
+
+  if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+    include $app_root . '/' . $site_path . '/settings.local.php';
+  }
